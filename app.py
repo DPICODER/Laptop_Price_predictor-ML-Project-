36075ec68e10df43518c8fc234cf38eaff507c3b
SaiVarun <varunsaipannala@gmail.com>
2023-05-26 11:57:11 +0530

    Layout_changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 pipe = pickle.load(open('pipe12.pkl','rb'))
 df = pickle.load(open('df.pkl','rb'))
 
-st.title("Laptop Predictor")
+st.title("Laptop Price Predictor")
 
 # brand
 company = st.selectbox('Brand',df['Company'].unique())
@@ -17,27 +17,42 @@ type = st.selectbox('Type',df['TypeName'].unique())
 # Ram
 ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
 
-# weight
-weight = st.number_input('Weight of the Laptop')
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    # Weight input
+    weight = st.number_input("Weight of laptop in kg")
 
-# Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+with middle_column:
+    # Touchscreen
+    touchscreen = st.selectbox("Touchscreen", ["No", "Yes"])
 
-# IPS
-ips = st.selectbox('IPS',['No','Yes'])
+with right_column:
+    # IPS display
+    ips = st.selectbox("IPS Display", ["No", "Yes"])
 
-# screen size
-screen_size = st.number_input('Screen Size')
+# making 3 cols left_column, middle_column, right_column
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    # screen size
+    Screen_size = st.number_input("Screen Size (in Inches)")
 
-# resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+with middle_column:
+    # resolution
+  resolution = st.selectbox('Screen Resolution',['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600','2560x1440', '2304x1440'])
+with right_column:
+    # cpu input
+    cpu = st.selectbox("CPU Brand", df["Cpu_brand"].unique())
 
-#cpu
-cpu = st.selectbox('CPU',df['Cpu_brand'].unique())
+# making 3 cols left_column, middle_column, right_column
+left_column,  right_column = st.columns(2)
+with left_column:
+    # hdd input
+    hdd = st.selectbox("HDD(in GB)", [0, 128, 256, 512, 1024, 2048])
 
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+with right_column:
+    # ssd input
+    ssd = st.selectbox("SSD(in GB)", [0, 8, 128, 256, 512, 1024])
 
 gpu = st.selectbox('GPU',df['Gpu_brand'].unique())
 
